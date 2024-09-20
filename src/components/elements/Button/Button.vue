dd281820e91ddd8f2e59142f6a5a8b8095a6fb04
vbatrakova <vbatrakova@tsconsulting.com>
2024-09-20 11:53:13 +0300

    refactor: Удалены ненужные файлы

--- src/components/elements/Button/Button.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <button :disabled="disabled">{{ buttonName }}</button>
-</template>
-<script lang="ts" setup>
-import { defineProps } from 'vue'
-import type { Props } from './types/types'
-const { buttonName = '', disabled = false } = defineProps<Props>()
-</script>
