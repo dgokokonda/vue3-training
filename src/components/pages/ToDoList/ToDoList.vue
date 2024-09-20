1400fb312ef4df20fefbbab46eb903478458361b
vbatrakova <vbatrakova@tsconsulting.com>
2024-09-20 11:21:18 +0300

    feat (Form Bindings): Сделана привязка к состоянию ввода формы в инпуте и чекбоксе

--- src/components/pages/ToDoList/ToDoList.vue
+++ src/components/pages/ToDoList/ToDoList.vue
@@ -2,17 +2,27 @@
   <div class="list-wrapper">
     <h4>Список действий:</h4>
     <div class="control-panel">
-      <input v-model="text" type="text" @keyup.enter="addText" class="editor" />
-      <button class="text-button" @click="addText">Добавить пункт</button>
+      <input v-model.trim="text" type="text" @keyup.enter="addText" @input="input" class="editor" />
+      <button class="text-button" :disabled="!text.length" @click="addText">Добавить пункт</button>
     </div>
-    <div class="list">{{ `Количество пунктов: ${todos.length}` }}</div>
+    <div class="list">{{ `Количество пунктов: ${todos.length}. Реактивный текст: ${text}` }}</div>
+    <input type="checkbox" id="adult" :value="isAdult" @input="checkAdult" />
+    <label for="adult">{{ `Я совершеннолетний: ${!isAdult ? 'Нет' : 'Да'}` }}</label>
   </div>
 </template>
 <script setup lang="ts">
 import { reactive, ref } from 'vue'
-import type { TextType, TodosType } from './types/types'
+import type { TextType, TodosType, CheckboxType } from './types/types'
 const text = ref<TextType>('')
 const todos = reactive<TodosType>([])
+const isAdult = ref<CheckboxType>(false)
+
+function input(e) {
+  console.log('input', e.target.value, text.value)
+}
+
+function checkAdult() {}
+isAdult.value = !isAdult.value
 
 function addText() {
   if (!text.value.length) return
@@ -43,6 +53,11 @@ function addText() {
     font-weight: bold;
     border: none;
     cursor: pointer;
+
+    &:disabled {
+      cursor: default;
+      opacity: 0.6;
+    }
   }
 
   .editor {
@@ -56,6 +71,10 @@ function addText() {
 
   .list {
     margin-top: 8px;
+  }
+
+  input[type='checkbox'] + label {
+    margin-left: 4px;
   }
 }
 </style>
--- src/components/pages/ToDoList/types/types.ts
+++ src/components/pages/ToDoList/types/types.ts
@@ -1,3 +1,5 @@
 export type TextType = string
 
 export type TodosType = { id: number; name: string }[]
+
+export type CheckboxType = boolean
